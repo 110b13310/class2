4661682031122fb413d4d312d3f49116ec3f8d5f
110b13310 <114041713+110b13310@users.noreply.github.com>
2022-09-28 11:30:09 +0800

    Create class08.java

---
+++ class08.java
@@ -0,0 +1,21 @@
+class CWin
+{
+ private static int cnt;
+ public static void count()
+ {
+  cnt=cnt+1; 
+  System.out.println(cnt+" objects");
+ }
+}
+public class eel
+{
+ 
+
+public static void main(String args[])
+  {
+ CWin a=new CWin();
+ CWin.count();
+ CWin b=new CWin();
+ CWin.count();
+  }
+}
