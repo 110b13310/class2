a4c7ffdd6b7a3ae21ecd1fa0c628e2bad86f73c5
110b13310 <114041713+110b13310@users.noreply.github.com>
2022-09-28 11:27:58 +0800

    Create class05.java

---
+++ class05.java
@@ -0,0 +1,57 @@
+class CCount
+{
+ static int cnt=0;
+ public CCount ()
+ {
+  
+  cnt ++;
+ }
+
+ public static void count()
+ {
+  
+  System.out.println(cnt+"object(s) created");
+ }
+}
+ public class eel
+ {
+  public static void main(String args[])
+  {
+   CCount a=new CCount();
+   CCount.count();
+   CCount b=new CCount();   
+   CCount c=new CCount();
+   CCount.count();
+   CCount.count();
+  }
+ }
+class CCount
+{
+ static int cnt=0;
+ public CCount ()
+ { 
+  cnt ++;
+ }
+
+ public  void count()
+ { 
+  System.out.println(cnt+"object(s) created");
+ }
+ void setValue()
+ {
+  cnt=0;
+ }
+}
+ public class eel
+ {
+  public static void main(String args[])
+  {
+   CCount a=new CCount();
+   a.count();
+   CCount b=new CCount();   
+   CCount c=new CCount();
+   b.count();
+   a.setValue();
+   c.count();
+  }
+ }
